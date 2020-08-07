95b30e3627c6c12221c8dba545adfdc2df929bb6
Chevalier Valentin <contact@chevalierv.be>
2020-08-07 19:00:17 +0200

    Remove venv from git

--- .TODODjango/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\200878696\Documents\Perso\TODODjango\.TODODjango\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
